38b78c303dfae4fd9e7b3acd9f22b64dc4cc49e2
Lucas Legendre <lucas.lucasleg@gmail.com>
2026-04-15 11:55:40 +0200

    implentation de la logique du clicker

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import { MoneyDisplay } from '@/components/MoneyDisplay'
 import { IncomeDisplay } from '@/components/IncomeDisplay'
 import { ClickButton } from '@/components/ClickButton'
 
 export function HomePage() {
+  const [money, setMoney] = useState(0)
+  const clickValue = 1
+
+  const handleClick = () => {
+    setMoney(money + clickValue)
+  }
   return (
     <section className="hero">
       <h1>Startup Tycoon</h1>
       <p className="subtitle">Ici vous jouerez au clicker.</p>
       <div className="counter">
-        <MoneyDisplay money={0} />
+        <MoneyDisplay money={money} />
         <IncomeDisplay income={0} />
       </div>
-      <ClickButton clickValue={1} onClick={() => {}} />
+      <ClickButton clickValue={clickValue} onClick={handleClick} />
     </section>
   )
 }
